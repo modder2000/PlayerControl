9ace0d1f2b75f6cea11d33873593d7b3d8067b55
Ayanna <palmsa1@roverkids.org>
2025-04-08 13:59:06 -0400

    completed 1.2

---
+++ PlayerControl/Assets/scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // we'll move the vechicle forward  
+
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
