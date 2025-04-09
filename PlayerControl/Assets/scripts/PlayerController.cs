cb64be321dbecae5f76fc2426be077f915d10795
Ayanna <palmsa1@roverkids.org>
2025-04-09 13:47:41 -0400

    completed 1.3

--- PlayerControl/Assets/scripts/PlayerController.cs
+++ PlayerControl/Assets/scripts/PlayerController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +16,6 @@ public class PlayerController : MonoBehaviour
     {
         // we'll move the vechicle forward  
 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
